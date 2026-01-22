b541e1020a2624418014f6c807c09b1414c9156c
SasikiranJ <sasikiran.jakkula@thoughtworks.com>
2026-01-22 19:00:11 +0530

    Added French translation support for order app

--- apps/orders/setupTests.i18n.ts
+++ apps/orders/setupTests.i18n.ts
@@ -1,6 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslations from './public/locales/locale_en.json';
+import esTranslations from './public/locales/locale_es.json';
+import frTranslations from './public/locales/locale_fr.json';
 
 const initTestI18n = () => {
   i18n.use(initReactI18next).init({
@@ -11,6 +13,8 @@ const initTestI18n = () => {
     defaultNS: 'orders',
     resources: {
       en: { orders: enTranslations },
+      es: { orders: esTranslations },
+      fr: { orders: frTranslations },
     },
     interpolation: {
       escapeValue: false,
