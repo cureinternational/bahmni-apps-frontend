4daafe2eeaa196335d5f53c2a5b64224164477ed
Pooja Sastry <pooja.sastry@thoughtworks.com>
2026-04-06 20:21:48 +0530

    Pooja | Update role name

--- packages/bahmni-services/src/orders/constants.ts
+++ packages/bahmni-services/src/orders/constants.ts
@@ -8,5 +8,5 @@ export const TAB_PRACTITIONER_TYPE_MAP: Record<string, string> = {
   'Rehab Order': 'Physiotherapist',
   'P&O Order': 'P%26O%20Technician',
   'Speech Therapy Order': 'Speech%20Therapist',
-  'Audiology Order': 'Audiology%20Technician',
+  'Audiology Order': 'Audiology%20technician',
 };
