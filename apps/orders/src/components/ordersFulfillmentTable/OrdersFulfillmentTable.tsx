465d0668f4c6ce7c688071fdac32ea020f6c7416
Suhas Katkuri <suhas.katkuri@thoughtworks.com>
2026-08-04 15:27:51 +0530

    Revert "Suhas| Hive-124712 |Expand Click Target for Status Filter-In order mo…"

--- apps/orders/src/components/ordersFulfillmentTable/OrdersFulfillmentTable.tsx
+++ apps/orders/src/components/ordersFulfillmentTable/OrdersFulfillmentTable.tsx
@@ -243,11 +243,7 @@ export const OrdersFulfillmentTable: React.FC<OrdersFulfillmentTableProps> = ({
         return {
           ...h,
           header: (
-            <span
-              ref={statusHeaderRef}
-              className={styles.statusHeader}
-              onClick={toggleStatusFilter}
-            >
+            <span ref={statusHeaderRef} className={styles.statusHeader}>
               {h.header}
               <svg
                 width="16"
@@ -257,6 +253,7 @@ export const OrdersFulfillmentTable: React.FC<OrdersFulfillmentTableProps> = ({
                 xmlns="http://www.w3.org/2000/svg"
                 className={styles.statusCaret}
                 aria-hidden="true"
+                onClick={toggleStatusFilter}
               >
                 <path
                   d="M4 6L8 10L12 6"
